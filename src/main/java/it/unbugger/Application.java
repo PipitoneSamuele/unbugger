e47dd60cecb0850c871caa0727bbaa9b04912bdf
PipitoneSamuele <samuele.pipitone@gmail.com>
2024-11-22 23:21:10 +0100

    user input and tests

--- src/main/java/it/unbugger/Application.java
+++ src/main/java/it/unbugger/Application.java
@@ -1,13 +1,22 @@
 package it.unbugger;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Application {
 
+	public static String path = "C:\\Users\\samue\\Desktop\\sw projects\\unbugger\\src\\test\\files";
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter path for file (it start from " + path + ")");
+		String s = scanner.nextLine();
+
+		System.out.println(path + s);
 	}
 
 }
