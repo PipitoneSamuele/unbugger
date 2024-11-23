1d766940ceb5134ce5018caed4083e571403f7d3
PipitoneSamuele <samuele.pipitone@gmail.com>
2024-11-23 12:34:30 +0100

    Added file reader and more menu section

--- src/main/java/it/unbugger/Application.java
+++ src/main/java/it/unbugger/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -22,10 +25,25 @@ public class Application {
 		File directory = new File(path+s);
 		File[] files = directory.listFiles();
 
-		if(files != null) {
-			for (File file : files) {
-				System.out.println(file.getName());
-			}
+		if(files == null) {
+			System.out.println("No files found in the choosen directory");
+			return;
+		}
+
+		System.out.println("Which file do you want to timerize? (write the number)");
+		int count = 0;
+		for (File file : files) {
+			System.out.println(++count + ") " + file.getName());
+		}
+
+		int chosenFileNumber = Integer.parseInt(scanner.nextLine());
+		File chosenFile = files[chosenFileNumber-1];
+		System.out.println("You chose: " + chosenFile.getName());
+
+		System.out.println("Your file contains: ");
+		List<String> fileStrings = Files.readAllLines(chosenFile.toPath(), StandardCharsets.UTF_8);
+		for(String fileLine : fileStrings) {
+			System.out.println(fileLine);
 		}
 	}
 
